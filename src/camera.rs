8a10ff2e72aa7fb083ed4656dcd743dba8017be3
Sal Rahman <salehen.rahman@gmail.com>
2022-06-05 12:21:26 -0700

    Done

--- src/camera.rs
+++ src/camera.rs
@@ -8,38 +8,71 @@ pub struct Camera {
   pub lower_left_corner: Vec3,
   pub horizontal: Vec3,
   pub vertical: Vec3,
+  pub u: Vec3,
+  pub v: Vec3,
+  pub w: Vec3,
+  pub lens_radius: f32,
 }
 
 impl Camera {
-  pub fn default() -> Camera {
-    Camera {
-      lower_left_corner: Vec3::new(-2.0, -1.0, -1.0),
-      horizontal: Vec3::new(4.0, 0.0, 0.0),
-      vertical: Vec3::new(0.0, 2.0, 0.0),
-      origin: Vec3::new(0.0, 0.0, 0.0),
-    }
-  }
+  // pub fn default() -> Camera {
+  //   Camera {
+  //     lower_left_corner: Vec3::new(-2.0, -1.0, -1.0),
+  //     horizontal: Vec3::new(4.0, 0.0, 0.0),
+  //     vertical: Vec3::new(0.0, 2.0, 0.0),
+  //     origin: Vec3::new(0.0, 0.0, 0.0),
+  //   }
+  // }
 
-  pub fn new(vfov: f32, aspect: f32) -> Camera {
+  pub fn new(
+    lookfrom: Vec3,
+    lookat: Vec3,
+    vup: Vec3,
+    vfov: f32,
+    aspect: f32,
+    aperture: f32,
+    focus_dist: f32,
+  ) -> Camera {
+    let origin = lookfrom;
+
+    let u: Vec3;
+    let v: Vec3;
+    let w: Vec3;
     let theta = vfov * std::f32::consts::PI / 180.0;
     let half_height = (theta / 2.0).tan();
     let half_width = aspect * half_height;
+    w = lookfrom.sub(lookat).unit_vector();
+    u = vup.cross(w);
+    v = w.cross(u);
+    // let mut lower_left_corner = Vec3::new(-half_width, -half_height, -1.0);
+    let lower_left_corner = origin
+      .sub(u.scalar_mul(half_width * focus_dist))
+      .sub(v.scalar_mul(half_height * focus_dist))
+      .sub(w.scalar_mul(focus_dist));
+
     Camera {
-      lower_left_corner: Vec3::new(-half_width, -half_height, -1.0),
-      horizontal: Vec3::new(2.0 * half_width, 0.0, 0.0),
-      vertical: Vec3::new(0.0, 2.0 * half_height, 0.0),
-      origin: Vec3::new(0.0, 0.0, 0.0),
+      lower_left_corner: lower_left_corner,
+      horizontal: u.scalar_mul(2.0 * half_width * focus_dist),
+      vertical: v.scalar_mul(2.0 * half_height * focus_dist),
+      origin: lookfrom,
+      u: u,
+      v: v,
+      w: w,
+      lens_radius: aperture / 2.0,
     }
   }
 
   pub fn get_ray(self, u: f32, v: f32) -> Ray {
+    let rd = Vec3::random_in_unit_sphere().scalar_mul(self.lens_radius);
+    let offset = self.u.scalar_mul(rd.e0).add(self.v.scalar_mul(rd.e1));
     Ray {
-      a: self.origin,
+      a: self.origin.add(offset),
       b: self
         .lower_left_corner
         .add(self.horizontal.scalar_mul(u))
         .add(self.vertical.scalar_mul(v))
-        .sub(self.origin),
+        .sub(self.origin)
+        .sub(offset),
     }
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -47,68 +47,111 @@ fn color<T: Hitable>(r: ray::Ray, world: &T, depth: u8) -> vec3::Vec3 {
     .add(Vec3::new(0.5, 0.7, 1.0).scalar_mul(t))
 }
 
+fn random_scene() -> HitableList {
+  let mut list: Vec<Box<dyn crate::hit_record::Hitable>> = Vec::new();
+  list.push(Box::new(sphere::Sphere::new(
+    Vec3::new(0.0, -1000.0, 0.0),
+    1000.0,
+    MaterialType::LambertianMat(Lambertian {
+      albedo: Vec3::new(0.5, 0.5, 0.5),
+    }),
+  )));
+
+  for a in -11..11 {
+    for b in -11..11 {
+      let choose_mat = rand::random::<f32>();
+      let center = Vec3::new(
+        (a as f32) + 0.9 * rand::random::<f32>(),
+        0.2,
+        (b as f32) + 0.9 * rand::random::<f32>(),
+      );
+      if center.sub(Vec3::new(4.0, 0.2, 0.0)).length() > 0.9 {
+        if choose_mat < 0.8 {
+          list.push(Box::new(sphere::Sphere::new(
+            center,
+            0.2,
+            MaterialType::LambertianMat(Lambertian {
+              albedo: Vec3::new(
+                rand::random::<f32>() * rand::random::<f32>(),
+                rand::random::<f32>() * rand::random::<f32>(),
+                rand::random::<f32>() * rand::random::<f32>(),
+              ),
+            }),
+          )));
+        } else if choose_mat < 0.95 {
+          list.push(Box::new(sphere::Sphere::new(
+            center,
+            0.2,
+            MaterialType::MetalMat(Metal {
+              fuzz: 0.5 * rand::random::<f32>(),
+              albedo: Vec3::new(
+                0.5 * (1.0 + rand::random::<f32>()),
+                0.5 * (1.0 + rand::random::<f32>()),
+                0.5 * (1.0 + rand::random::<f32>()),
+              ),
+            }),
+          )));
+        } else {
+          list.push(Box::new(sphere::Sphere::new(
+            center,
+            0.2,
+            MaterialType::DialectricMat(Dialectric { ref_idx: 1.5 }),
+          )));
+        }
+      }
+    }
+  }
+
+  list.push(Box::new(sphere::Sphere::new(
+    Vec3::new(0.0, 1.0, 0.0),
+    1.0,
+    MaterialType::DialectricMat(Dialectric { ref_idx: 1.5 }),
+  )));
+
+  list.push(Box::new(sphere::Sphere::new(
+    Vec3::new(-4.0, 1.0, 0.0),
+    1.0,
+    MaterialType::LambertianMat(Lambertian {
+      albedo: Vec3::new(0.4, 0.2, 0.1),
+    }),
+  )));
+
+  list.push(Box::new(sphere::Sphere::new(
+    Vec3::new(4.0, 1.0, 0.0),
+    1.0,
+    MaterialType::MetalMat(Metal {
+      fuzz: 0.0,
+      albedo: Vec3::new(0.7, 0.6, 0.5),
+    }),
+  )));
+
+  hitable_list::HitableList { list }
+}
+
 fn main() {
-  let nx = 200;
-  let ny = 100;
+  let nx = 1024;
+  let ny = 576;
   let ns = 100;
   println!("P3");
   println!("{0} {1}", nx, ny);
   println!("255\n");
 
-  let mut list: Vec<Box<dyn crate::hit_record::Hitable>> = Vec::new();
+  let mut world = random_scene();
 
-  // list.push(Box::new(sphere::Sphere::new(
-  //   Vec3::new(0.0, 0.0, -1.0),
-  //   0.5,
-  //   MaterialType::LambertianMat(Lambertian {
-  //     albedo: Vec3::new(0.8, 0.3, 0.3),
-  //   }),
-  // )));
-  // list.push(Box::new(sphere::Sphere::new(
-  //   Vec3::new(0.0, -100.5, -1.0),
-  //   100.0,
-  //   MaterialType::LambertianMat(Lambertian {
-  //     albedo: Vec3::new(0.8, 0.8, 0.0),
-  //   }),
-  // )));
-  // list.push(Box::new(sphere::Sphere::new(
-  //   Vec3::new(1.0, 0.0, -1.0),
-  //   0.5,
-  //   MaterialType::MetalMat(Metal {
-  //     albedo: Vec3::new(0.8, 0.6, 0.2),
-  //     fuzz: 0.3,
-  //   }),
-  // )));
-  // list.push(Box::new(sphere::Sphere::new(
-  //   Vec3::new(-1.0, 0.0, -1.0),
-  //   0.5,
-  //   MaterialType::DialectricMat(Dialectric { ref_idx: 1.5 }),
-  // )));
-  // list.push(Box::new(sphere::Sphere::new(
-  //   Vec3::new(-1.0, 0.0, -1.0),
-  //   -0.45,
-  //   MaterialType::DialectricMat(Dialectric { ref_idx: 1.5 }),
-  // )));
+  let lookfrom = Vec3::new(12.0, 2.0, 4.0);
+  let lookat = Vec3::new(0.0, 1.0, 0.0);
+  let dist_to_focus = lookfrom.sub(lookat).length();
+  let aperture = 0.2;
 
-  let r = std::f32::consts::PI / 4.0;
-  list.push(Box::new(sphere::Sphere::new(
-    Vec3::new(-r, 0.0, -1.0),
-    r,
-    MaterialType::LambertianMat(Lambertian {
-      albedo: Vec3::new(0.0, 0.0, 1.0),
-    }),
-  )));
-  list.push(Box::new(sphere::Sphere::new(
-    Vec3::new(r, 0.0, -1.0),
-    r,
-    MaterialType::LambertianMat(Lambertian {
-      albedo: Vec3::new(1.0, 0.0, 0.0),
-    }),
-  )));
-
-  let mut world = HitableList { list: list };
-
-  let cam = Camera::new(90.0, (nx as f32) / (ny as f32));
+  let cam = Camera::new(
+    lookfrom,
+    lookat,
+    Vec3::new(0.0, 1.0, 0.0),
+    20.0,
+    (nx as f32) / (ny as f32),
+    aperture,
+    dist_to_focus,
+  );
 
   for j in (0..ny).rev() {
     for i in 0..nx {
